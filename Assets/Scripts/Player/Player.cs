4d6834ee18d61813a637ee97fe6dc261f74a78d7
Dogancan Cedetas <dogancancedetas@hotmail.com>
2021-12-03 16:39:45 +0300

    Karakter'e yön hareketleri eklendi

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))] //parantez içine yazdýðýmýz component'ý zorunlu kýlar kaldýrmayý engeller. Bulunan scripti eklerseniz component'ý ekler
 public class Player : MonoBehaviour
 {
+    
+    Rigidbody2D body2D;
+    public float playerSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        body2D = GetComponent<Rigidbody2D>();
+        body2D.gravityScale = 50;
+        body2D.freezeRotation = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    // FixedUpdate: Framerate'den baðýmsýz olarak çalýþýr. Fizik ile ilgili kodlarý buraya yazýn.
+    void FixedUpdate()
     {
-        
+        float h = Input.GetAxis("Horizontal");
+       
+        body2D.velocity = new Vector2(h * playerSpeed, 0);
     }
 }
