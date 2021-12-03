f5a500ee03ba20cd4863d5d2a6357c28f78c5762
Dogancan Cedetas <dogancancedetas@hotmail.com>
2021-12-03 17:29:22 +0300

    zıplama eklendi, sonsuz zıplama engellendi

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,27 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))] //parantez içine yazdýðýmýz component'ý zorunlu kýlar kaldýrmayý engeller. Bulunan scripti eklerseniz component'ý ekler
+[RequireComponent(typeof(Rigidbody2D))] //parantez icine yazdigimiz component'i zorunlu kilar kaldirmayi engeller. Bulunan scripti eklerseniz component'i ekler
 public class Player : MonoBehaviour
 {
-    
+    //Rigidbody2D
     Rigidbody2D body2D;
+
+    /// <summary>
+    /// bu var'lar karakterin hizini ve ziplama gucunu belirler
+    /// </summary>
+    [Tooltip("Karakterin ne kadar hizli gidecegini belirler.")] // uzerine gelindiginde bir tooltip cikartir
+    [Range(0, 20)] 
     public float playerSpeed;
+
+    [Tooltip("Karakterin ne kadar yuksege ziplayacagini belirler.")]
+    [Range(0,1500)]
+    public float jumpForce;
+
+
+    //Yeri bulma
+    [Tooltip("Karakterin yere degip degmedigini kontrol eder.")]
+    public bool isOnGround;
+    Transform groundCheck;
+    const float groundCheckRadius = 0.2f;
+    [Tooltip("Yerin ne oldugunu belirler.")]
+    public LayerMask groundLayer;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        // Rigidbody ayarlari
         body2D = GetComponent<Rigidbody2D>();
-        body2D.gravityScale = 50;
+        body2D.gravityScale = 5;
         body2D.freezeRotation = true;
+        body2D.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+
+        //GrouncCheck'i bul.
+        groundCheck = transform.Find("GroundCheck");
     }
 
     
-    // FixedUpdate: Framerate'den baðýmsýz olarak çalýþýr. Fizik ile ilgili kodlarý buraya yazýn.
+    // FixedUpdate: Framerate'den bagimsiz olarak calisir. Fizik ile ilgili kodlari buraya yazin.
     void FixedUpdate()
     {
+        isOnGround =  Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
         float h = Input.GetAxis("Horizontal");
        
-        body2D.velocity = new Vector2(h * playerSpeed, 0);
+        body2D.velocity = new Vector2(h * playerSpeed, body2D.velocity.y);
+    }
+
+    public void Jump()
+    {
+        body2D.AddForce(new Vector2(0, jumpForce));
+        isOnGround = false;
     }
 }
--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
+    Player player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GetComponent<Player>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Jump") && player.isOnGround)
+        {
+            player.Jump();
+        }
     }
 }
